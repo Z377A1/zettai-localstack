45e9eb0f5f3cdc76aac30499189f50b706e50a00
Z377A1 <70942307+Z377A1@users.noreply.github.com>
2025-08-22 18:29:00 +0200

    Update start.sh to reflect deprecation of LOCALSTACK_HOSTNAME and add logs-tail.sh for S3 cost calculation

--- .devcontainer/scripts/start.sh
+++ .devcontainer/scripts/start.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-# Set the LocalStack hostname environment variable
+# Set the LocalStack host environment variable
+# 2025-08-22T16:23:02.334  WARN --- [  MainThread] localstack.deprecations    :
+# LOCALSTACK_HOSTNAME is deprecated (since 2.0.0) 
+# and will be removed in upcoming releases of LocalStack! 
+# This configuration will be migrated to LOCALSTACK_HOST
 export LOCALSTACK_HOSTNAME="$(hostname)"
+# export LOCALSTACK_HOST=$LOCALSTACK_HOSTNAME
 
 # Start localstack
 localstack start -d
---
+++ logs-tail.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Calculate S3 Object Storage costs for Hetzner vs Cloudflare R2
+
+set -e
+
+localstack logs -f
